96aebf01e7a2ffe7aa573367d41f8b539a4b51a3
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-04-26 09:05:18 +0000

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config'
 import node from '@astrojs/node'
 import alpinejs from '@astrojs/alpinejs'
 import htmx from 'astro-htmx'
+import tailwind from '@astrojs/tailwind'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.hanyauntukdisyukuri.my.id',
-  integrations: [alpinejs(), htmx()],
+  integrations: [alpinejs(), htmx(), tailwind()],
   image: {
     domains: [
       'hanyauntukdisyukuri.my.id',
@@ -18,4 +20,11 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone',
   }),
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
 })
