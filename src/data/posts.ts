562b59e0373a45f33f5f1f9b3c7bc9bc7f82de58
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-04-29 16:45:39 +0000

    WIP

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -50,11 +50,11 @@ type PostsByCategoryFetched = {
   data: Pick<RootQuery, 'posts' | 'category'>
 }
 
-export const fetchPostsByCategory = async (id: string) => {
+export const fetchPostsByCategory = async (id: string, first?: number) => {
   return (await fetchData({
     query: gql`
-      query FetchPostsByCategory($id: ID!, $category: String!) {
-        posts(where: { categoryName: $category }, first: 6) {
+      query FetchPostsByCategory($id: ID!, $category: String!, $first: Int!) {
+        posts(where: { categoryName: $category }, first: $first) {
           nodes {
             title
             excerpt
@@ -77,7 +77,44 @@ export const fetchPostsByCategory = async (id: string) => {
     `,
     variables: {
       id,
+      first: first || 6,
       category: id,
     },
   })) as PostsByCategoryFetched
+}
+
+type PostsByUserFetched = {
+  data: Pick<RootQuery, 'posts' | 'user'>
+}
+
+export const fetchPostsByUser = async (id: string) => {
+  return (await fetchData({
+    query: gql`
+      query FetchPostsByUser($id: ID!, $user: String!) {
+        posts(where: { authorName: $user }, first: 12) {
+          nodes {
+            title
+            excerpt
+            uri
+            date
+            featuredImage {
+              node {
+                sourceUrl
+                altText
+                caption
+              }
+            }
+          }
+        }
+        user(id: $id, idType: SLUG) {
+          name
+          uri
+        }
+      }
+    `,
+    variables: {
+      id,
+      user: id,
+    },
+  })) as PostsByUserFetched
 }
