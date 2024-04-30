84e44ded5ffb05b77ee6665b98a9141b7c6e896d
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-04-30 08:22:24 +0000

    WIP

---
+++ src/data/pages.ts
@@ -0,0 +1,30 @@
+import type { RootQuery } from './graphql'
+import { fetchData, gql } from './fetcher'
+
+type PageFetched = {
+  data: Pick<RootQuery, 'page'>
+}
+
+export const fetchPage = async (id: string) => {
+  return (await fetchData({
+    query: gql`
+      query FetchPage($id: ID!) {
+        page(id: $id, idType: URI) {
+          title
+          uri
+          content
+          featuredImage {
+            node {
+              sourceUrl
+              caption
+              altText
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      id,
+    },
+  })) as PageFetched
+}
