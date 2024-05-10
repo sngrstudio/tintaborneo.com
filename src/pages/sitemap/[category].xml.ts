d3faa1dd1105f1ca2d48ffe03d020a72fd660319
radenpioneer <7355835+radenpioneer@users.noreply.github.com>
2024-05-10 23:24:52 +0700

    WIP

--- src/pages/sitemap/[category].xml.ts
+++ src/pages/sitemap/[category].xml.ts
@@ -20,18 +20,16 @@ export const GET: APIRoute = async ({ site, params, redirect }) => {
   const xml = `
     <?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="style.xsl"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-      ${posts?.nodes
-        .map(
-          (post: Post) => `
+      ${posts?.nodes.map(
+        (post: Post) => `
           <url>
-            <loc>${site?.origin}${post.uri}</loc>
+            <loc>${encodeURI(`${site?.origin}${post.uri}`)}</loc>
             <lastmod>${post?.date}+07:00</lastmod>
           </url>
         `
-        )
-        .join('')}
+      )}
     </urlset>    
-  `.replace(/\s+/g, '')
+  `.trim()
 
   return new Response(xml, {
     headers: {
