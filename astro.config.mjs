26362bf8e128d20531e4138b475be27b62a99fdc
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-24 09:20:21 +0000

    RESET

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,25 @@
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 import alpinejs from '@astrojs/alpinejs'
 import react from '@astrojs/react'
 import Icons from 'unplugin-icons/vite'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.tintaborneo.com',
+  site: 'https://next.tintaborneo.com',
   integrations: [tailwind(), alpinejs(), react()],
   image: {
     domains: [
       'tintaborneo.com',
       'www.tintaborneo.com',
       'staging.tintaborneo.com',
-      'content.tintaborneo.com'
+      'content.tintaborneo.com',
+      'next.tintaborneo.com'
     ]
   },
   output: 'server',
-  adapter: node({
-    mode: 'middleware'
-  }),
+  adapter: cloudflare(),
   vite: {
     plugins: [
       Icons({
