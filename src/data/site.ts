9a3d7b7db0f02018707c759c7e7add09b81afcab
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-24 10:11:44 +0000

    WIP

---
+++ src/data/site.ts
@@ -0,0 +1,24 @@
+import type { RootQuery } from './graphql'
+import { fetchQuery, type FetchQueryResult } from './fetch'
+
+type SiteInfoResult = FetchQueryResult<
+  Required<Pick<RootQuery, 'generalSettings'>>
+>
+type LogoResult = FetchQueryResult<Required<Pick<RootQuery, 'mediaItem'>>>
+
+export const getSiteInfo = async () => {
+  const res = (await fetchQuery({
+    queryId: 'get-site-info'
+  })) as SiteInfoResult
+  return res.data.generalSettings!
+}
+
+export const getLogo = async (id: string) => {
+  const res = (await fetchQuery({
+    queryId: 'get-logo',
+    variables: {
+      id
+    }
+  })) as LogoResult
+  return res.data.mediaItem
+}
