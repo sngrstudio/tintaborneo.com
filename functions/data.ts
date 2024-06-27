55afbd21b9e96dc2bed45b58fa410c8819feb064
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-27 09:46:13 +0000

    WIP

--- functions/data.ts
+++
@@ -1,9 +0,0 @@
-interface Env {}
-
-export const onRequest: PagesFunction<Env> = () => {
-  return new Response(JSON.stringify({ hello: 'world!' }), {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-}
