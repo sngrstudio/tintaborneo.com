9cca30afc0dda78b17faab5b1df8242173af086f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-29 12:12:57 +0000

    WIP

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,7 +1,18 @@
-import type { RootQuery } from '~/graphql/types'
+import type { RootQuery, Post } from '~/graphql/types'
 import { fetchQuery, type FetchQueryResult } from './fetch'
 
 type PostResult = FetchQueryResult<Pick<RootQuery, 'post'>>
+type PostsResult = FetchQueryResult<Pick<RootQuery, 'posts'>>
 
 export const getPost = async (id: string) =>
   ((await fetchQuery('get-post', { id })) as PostResult).data.post
+
+export const getRelatedPosts = async ({
+  notIn,
+  tagIn
+}: {
+  notIn: Array<string>
+  tagIn: Array<string>
+}) =>
+  ((await fetchQuery('get-related-posts', { notIn, tagIn })) as PostsResult)
+    .data.posts?.nodes as Array<Post>
