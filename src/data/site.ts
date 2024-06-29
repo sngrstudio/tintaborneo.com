6be28f8b75b52f15765db6823425e68b7d27c5f9
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-29 09:22:28 +0000

    WIP

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,11 +1,16 @@
-import type { RootQuery } from '~/graphql/types'
+import type { MenuItem, MenuLocationEnum, RootQuery } from '~/graphql/types'
 import { fetchQuery, type FetchQueryResult } from './fetch'
 
 type SiteInfoResult = FetchQueryResult<Pick<RootQuery, 'generalSettings'>>
 type LogoResult = FetchQueryResult<Pick<RootQuery, 'mediaItem'>>
+type MenuItemsResult = FetchQueryResult<Pick<RootQuery, 'menuItems'>>
 
 export const getSiteInfo = async () =>
   ((await fetchQuery('get-site-info')) as SiteInfoResult).data.generalSettings!
 
 export const getLogo = async (id: string) =>
   ((await fetchQuery('get-logo', { id })) as LogoResult).data.mediaItem!
+
+export const getMenu = async (location: MenuLocationEnum) =>
+  ((await fetchQuery('get-menu', { location })) as MenuItemsResult).data
+    .menuItems?.nodes as Array<MenuItem>
