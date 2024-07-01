4049ca51d8ac52da6532016306935cc07b6d8e4f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-07-01 11:36:45 +0000

    WIP

--- src/data/taxonomies.ts
+++ src/data/taxonomies.ts
@@ -2,6 +2,11 @@ import type { RootQuery } from '~/graphql/types'
 import { fetchQuery, type FetchQueryResult } from './fetch'
 
 type TagResult = FetchQueryResult<Pick<RootQuery, 'tag'>>
+type CategoryResult = FetchQueryResult<Pick<RootQuery, 'category'>>
 
 export const getTag = async (id: string) =>
   ((await fetchQuery('get-tag-id', { id })) as TagResult).data.tag
+
+export const getCategory = async (id: string) =>
+  ((await fetchQuery('get-category-id', { id })) as CategoryResult).data
+    .category
