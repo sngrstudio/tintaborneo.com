586489ac1411a1094a8ac68b767b7c2965490e9f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-07-04 05:56:39 +0000

    WIP

---
+++ src/pages/og/home.png.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from 'astro'
+import type { ReactNode } from 'react'
+import satori from 'satori'
+import { html } from 'satori-html'
+import { Resvg, initWasm } from '@resvg/resvg-wasm'
+
+const SourceSerifProFontData = await fetch(
+  'https://cdn.jsdelivr.net/fontsource/fonts/source-serif-pro@latest/latin-400-normal.ttf'
+).then((res) => res.arrayBuffer())
+
+export const GET: APIRoute = async () => {
+  // initialize resvg wasm
+  try {
+    await initWasm('https://unpkg.com/@resvg/resvg-wasm/index_bg.wasm')
+  } catch (e) {
+    console.error(e)
+  }
+
+  const markup = html`
+    <div style="display: flex; width: 100%; height: 100%;">
+      <span style="font-family: 'Source Serif Pro'; font-weight: 400;">
+        Hello World!
+      </span>
+    </div>
+  ` as ReactNode
+
+  const svg = await satori(markup, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: 'Source Serif Pro',
+        data: SourceSerifProFontData,
+        style: 'normal'
+      }
+    ]
+  })
+  const png = new Resvg(svg).render().asPng()
+
+  return new Response(png, {
+    headers: {
+      'Content-Type': 'image/png'
+    }
+  })
+}
