e18f296ebd44b2f911285c4a64929b7ad4cda3fa
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-08-27 18:11:57 +0000

    disable sharp (again)

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import node from '@astrojs/node'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
@@ -14,6 +14,7 @@ export default defineConfig({
   integrations: [react()],
 
   image: {
+    service: passthroughImageService(),
     responsiveStyles: true,
     remotePatterns: [
       {
