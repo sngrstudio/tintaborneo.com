0165f18a78d937cbab471ff37db036b058a92378
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-09-10 03:12:48 +0000

    switch to npm, switch docker images, remove ladle

--- web/.ladle/components.tsx
+++
@@ -1,6 +0,0 @@
-import type { GlobalProvider } from '@ladle/react'
-import '../src/layouts/global.css'
-
-export const Provider: GlobalProvider = ({ children }) => {
-  return <>{children}</>
-}
--- web/.ladle/config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('@ladle/react').UserConfig} */
-export default {
-  port: 4321,
-  previewPort: 4321,
-  hmrPort: 43210,
-  host: '0.0.0.0',
-  previewHost: '0.0.0.0',
-  viteConfig: process.cwd() + '/.ladle/vite.config.mjs'
-}
